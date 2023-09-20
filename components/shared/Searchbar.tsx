1a6e84056836204f83412c8e7567eb2b7f7f7e5e
Bruce <nbruce420@gmail.com>
2023-09-20 18:15:44 +0200

    changed search

--- components/shared/Searchbar.tsx
+++ components/shared/Searchbar.tsx
@@ -7,46 +7,48 @@ import { useEffect, useState } from "react";
 import { Input } from "../ui/input";
 
 interface Props {
-  routeType: string;
+   routeType: string;
 }
 
 function Searchbar({ routeType }: Props) {
-  const router = useRouter();
-  const [search, setSearch] = useState("");
+   const router = useRouter();
+   const [search, setSearch] = useState("");
 
-  // query after 0.3s of no input
-  useEffect(() => {
-    const delayDebounceFn = setTimeout(() => {
-      if (search) {
-        router.push(`/${routeType}?q=` + search);
-      } else {
-        router.push(`/${routeType}`);
-      }
-    }, 300);
+   // query after 0.3s of no input
+   useEffect(() => {
+      const delayDebounceFn = setTimeout(() => {
+         if (search) {
+            router.push(`/${routeType}?q=` + search);
+         } else {
+            router.push(`/${routeType}`);
+         }
+      }, 300);
 
-    return () => clearTimeout(delayDebounceFn);
-  }, [search, routeType]);
+      return () => clearTimeout(delayDebounceFn);
+   }, [search, routeType]);
 
-  return (
-    <div className='searchbar'>
-      <Image
-        src='/assets/search-gray.svg'
-        alt='search'
-        width={24}
-        height={24}
-        className='object-contain'
-      />
-      <Input
-        id='text'
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder={`${
-          routeType !== "/search" ? "Search communities" : "Search creators"
-        }`}
-        className='no-focus searchbar_input'
-      />
-    </div>
-  );
+   return (
+      <div className="searchbar">
+         <Image
+            src="/assets/search-gray.svg"
+            alt="search"
+            width={24}
+            height={24}
+            className="object-contain"
+         />
+         <Input
+            id="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder={`${
+               routeType !== "/search"
+                  ? "Search communities"
+                  : "Search creators"
+            }`}
+            className="no-focus searchbar_input"
+         />
+      </div>
+   );
 }
 
 export default Searchbar;
